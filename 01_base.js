364d4b3a23ba53c7b89c36b686f44ec4d01a5a31
maxim.malyavko <maxim.malyavko@foxtree.com>
2020-05-18 11:46:07 +0300

    BigInt

---
+++ 01_base.js
@@ -0,0 +1,157 @@
+/********1 ПЕРЕМЕННЫЕ*********/
+
+//var first_name = "Maxim" // не использовать var
+//let first_name = "Maxim" // не использовать var
+//const last_name = "Malyavko" // read only string
+//let age = 35 // number
+//const isProgrammer = true; //boolean
+
+//нет строгой типизации
+//style guide -> camelCase
+// споры как писать с ; или без. Js позволяет и без - локоничным, чистым, в 99,99% они не нужны
+
+
+//const _private = 'private'
+//const $ = 'same value'
+
+/********2 МУТИРОВАНИЕ************/
+//лучше использовать одинарные ковычки - элегантнее
+//если одна из переменных строка, то сработате конкатенация - все приводится к строке => age.toString()
+// фиолетовый - это Number
+//console, alert = это фишки браузера
+
+//console.log("name of men: " + first_name)
+//console.log("age: " + age)
+//console.log(age)
+//const enterEge = prompt("Enter sip: ")
+//console.log(enterEge)
+
+/********3 ОПЕРАТОРЫ************/
+
+//const currentYear = 2020
+//const birthYear = 1985
+//let myEge = 2020 - 1985
+//console.log(currentYear++) // вот тут вот еще выведется 2020 - попорядку - вывели старое значение, а после увеличили
+//console.log(++currentYear) // вот тут вот еще выведется 2020 - попорядку - вывели новое значение, а после вывели
+//console.log(currentYear) // 2021
+
+/********4 ТИПЫ ДАННЫХ************/
+// примитивы string, number, boolean, undefined, null
+
+/* const isProgrammer = true
+const name = "Maxim"
+const age = 26
+let x
+console.log(typeof isProgrammer)
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof x)
+console.log(typeof null) // => object - bug */
+
+
+
+/********5 ПРИОРИТЕТ ОПЕРАЦИЙ************/
+/* const fullAge = 35
+const birth = 1985
+const currentYear = 2020
+
+// > < >= <=
+//()- приоритет самый высокий 20 баллов => mdn приоритет операторов
+// <== - приоритет 11, +- - приоритет 13
+const isFullAge = currentYear - birth >= fullAge */
+
+
+
+/********6 УСЛОВНЫЕ ОПЕРАТОРЫ************/
+/*const coursStatus = 'pending'
+
+ if (coursStatus === 'ready') {
+  console.log('курс уже готов и его можно проходить')
+} else if (coursStatus === 'pending') {
+  console.log('курс только разрабатывается')
+} else {
+  console.log('курс не получился')
+} */
+
+//const isReady = true
+
+/* if(isReady) {
+  console.log("все готово")
+} */
+
+//вместо if используются тернарные выражения, : - это else
+//isReady ? console.log(""):
+
+/* const num1 = 42
+const num2 = '42'
+console.log(num1 == num2) // так как == то приводится все к одному типу данных - в данном случае к строке
+console.log(num1 === num2) // так как === проверяет по типу */
+
+
+
+/********7 БУЛЕВАЯ ЛОГИКА************/
+// || && - возвращают последний, если всё условие правда
+// || - запинается на правде. Будет перебирать все, пока не встретить правду
+// && - запинается на лжи. Будет перебирать все, пока не встретить ложь
+
+// !true === false, !!true === true
+
+
+/********8 ФУНКЦИИ************/
+/* function calcAge(year) {
+  return 2020 - year
+}
+
+/* console.log(calcAge(1993))  */
+// композиции из функций
+
+/*function infoAbout(name, year){
+  let age = calcAge(year);
+  age>0?console.log("Имя персоны - ", name, age): console.log("что-то")
+}
+
+infoAbout("Maxo", 2010) */
+
+
+/********9 МАССИВЫ************/
+//это тип Обьектов (typeof [] - object), в которых в качестве ключей используются чиста: 1: "Mazda"
+//const cars = ["Mazda", "VW", "Ford"]
+//const cars = new Array("Mazda", "VW", "Ford") - вот так не стоит
+/* cars[0] = "Porshe"
+cars[cars.length] = "Mazda"
+console.log(cars)
+console.log(cars[1])
+console.log(cars.length) */
+
+
+/********10 ЦИКЛЫ************/
+/* for (let i = 0; i < cars.length; i++) {
+  console.log(cars[i])
+} */
+
+/* for (let car of cars){
+  console.log(car) 
+} */
+
+
+/********11 ОБЬЕКТЫ************/
+// на них все устроено
+//const person  = new Object(); // так можно, но не нужно
+/* const person = {
+  firstName: "Maxim",
+  lastName: "Maluavko",
+  year: 1993,
+  languages: ["RU", "EN", "Du"],
+  isProgramer: false,
+  greet: function () { console.log("GREET") }
+}
+
+
+//console.log(person.firstName)
+//console.log(person['lastName'])
+let key = 'languages'
+//console.log(person[key])
+person.isProgramer = true
+person.isWife = true    // add new property
+person.greet()
+console.log(person) */
--- app.js
+++ app.js
@@ -1,157 +1,39 @@
-/********1 ПЕРЕМЕННЫЕ*********/
+/********NUMBER types*********/
+/* const num = 42 // integer
+const float = 42.42// float
+const pow = 10e3  // 10 в 3 степени */
+
+// за кулисами это класс Number
+//Number.MAX_SAFE_INTEGER - максимально число - выше- несовсем корректные вычисления 2 в 53 степени минус 1
+//Number.MIN_SAFE_INTEGER - 2 в 53 +1
+//Number.MAX_VALUE - 5 в степени 324 степени. Но вычислеть мы можем только интеджер
+//Number.POSITIVE_INFINITY - положительная бесконечность
+//Number.NEGATIVE_INFINITY - отридцательная бесконечность
+//1/0 - бесконечность
+//Number.NaN - /not a number - специальное значение, которое не является числом, но type of NAN - number, 2/undefined
+// isNuN(2/undefined)=== true, Number.isNuN
+//Number.isFinite(Infinity) - false - является ли число конечным
+
+/* console.log('42' + 2) //= 422, так как у строки нет сложения, а только конкатенация.
+console.log(Number.parseInt('40') + 2) // можно просто parseInt('42')
+console.log(Number('40') + 2) 
+console.log(+'40' + 2) 
+console.log(Number.parseFloat(40.42) + 2) 
+console.log(+parseFloat(40.42) + 2)  */
 
 
-//var first_name = "Maxim" // не использовать var
-//let first_name = "Maxim" // не использовать var
-//const last_name = "Malyavko" // read only string
-//let age = 35 // number
-//const isProgrammer = true; //boolean
-
-//нет строгой типизации
-//style guide -> camelCase
-// споры как писать с ; или без. Js позволяет и без - локоничным, чистым, в 99,99% они не нужны
-
-
-//const _private = 'private'
-//const $ = 'same value'
-
-/********2 МУТИРОВАНИЕ************/
-//лучше использовать одинарные ковычки - элегантнее
-//если одна из переменных строкаб то сработате конкатенация - все приводится к строке => age.toString()
-// фиолетовый - это Number
-//console, alert = это фишки браузера
-
-//console.log("name of men: " + first_name)
-//console.log("age: " + age)
-//console.log(age)
-//const enterEge = prompt("Enter sip: ")
-//console.log(enterEge)
-
-/********3 ОПЕРАТОРЫ************/
-
-//const currentYear = 2020
-//const birthYear = 1985
-//let myEge = 2020 - 1985
-//console.log(currentYear++) // вот тут вот еще выведется 2020 - попорядку - вывели страрое значение, а после увеличили
-//console.log(++currentYear) // вот тут вот еще выведется 2020 - попорядку - вывели новое значение, а после вывели
-//console.log(currentYear) // 2021
-
-/********4 ТИПЫ ДАННЫХ************/
-// примитивы string, number, boolean, undefined, null
-
-/* const isProgrammer = true
-const name = "Maxim"
-const age = 26
-let x
-console.log(typeof isProgrammer)
-console.log(typeof name)
-console.log(typeof age)
-console.log(typeof x)
-console.log(typeof null) // => object - bug */
+/* console.log(0.4 + 0.2) // 0.6000000000000000000001 так как числа храняться в битовой.....
+console.log(+(0.4 + 0.2).toFixed(1)) */
 
 
 
-/********5 ПРИОРИТЕТ ОПЕРАЦИЙ************/
-/* const fullAge = 35
-const birth = 1985
-const currentYear = 2020
 
-// > < >= <=
-//()- приоритет самый высокий 20 баллов => mdn приоритет операторов
-// <== - приоритет 11, +- - приоритет 13
-const isFullAge = currentYear - birth >= fullAge */
+/********BigInt*********/
+//новый тип данных, больше чем MAX_SAFE_INTEGER
 
-
-
-/********6 УСЛОВНЫЕ ОПЕРАТОРЫ************/
-/*const coursStatus = 'pending'
-
- if (coursStatus === 'ready') {
-  console.log('курс уже готов и его можно проходить')
-} else if (coursStatus === 'pending') {
-  console.log('курс только разрабатывается')
-} else {
-  console.log('курс не получился')
-} */
-
-//const isReady = true
-
-/* if(isReady) {
-  console.log("все готово")
-} */
-
-//вместо if используются тернарные выражения, : - это else
-//isReady ? console.log(""):
-
-/* const num1 = 42
-const num2 = '42'
-console.log(num1 == num2) // так как == то приводится все к одному типу данных - в данном случае к строке
-console.log(num1 === num2) // так как === проверяет по типу */
-
-
-
-/********7 БУЛЕВАЯ ЛОГИКА************/
-// || && - возвращают последний, если всё условие правда
-// || - запинается на правде. Будет перебирать все, пока не встретить правду
-// && - запинается на лжи. Будет перебирать все, пока не встретить ложь
-
-// !true === false, !!true === true
-
-
-/********8 ФУНКЦИИ************/
-/* function calcAge(year) {
-  return 2020 - year
-}
-
-/* console.log(calcAge(1993))  */
-// композиции из функций
-
-/*function infoAbout(name, year){
-  let age = calcAge(year);
-  age>0?console.log("Имя персоны - ", name, age): console.log("что-то")
-}
-
-infoAbout("Maxo", 2010) */
-
-
-/********9 МАССИВЫ************/
-//const cars = ["Mazda", "VW", "Ford"]
-//const cars = new Array("Mazda", "VW", "Ford") - вот так не стоит
-/* cars[0] = "Porshe"
-cars[cars.length] = "Mazda"
-console.log(cars)
-console.log(cars[1])
-console.log(cars.length) */
-
-
-/********10 ЦИКЛЫ************/
-/* for (let i = 0; i < cars.length; i++) {
-  console.log(cars[i])
-} */
-
-/* for (let car of cars){
-  console.log(car) 
-} */
-
-
-/********11 ОБЬЕКТЫ************/
-// на них все устроено
-//const person  = new Object(); // так можно, но не нужно
-/* const person = {
-  firstName: "Maxim",
-  lastName: "Maluavko",
-  year: 1993,
-  languages: ["RU", "EN", "Du"],
-  isProgramer: false,
-  greet: function () { console.log("GREET") }
-}
-
-
-//console.log(person.firstName)
-//console.log(person['lastName'])
-let key = 'languages'
-//console.log(person[key])
-person.isProgramer = true
-person.isWife = true    // add new property
-person.greet()
-console.log(person) */
+/* console.log(typeof 90071992547409919999999n) // add n to finish
+// roole - bigInt работате только с bigInt, нет десятичных, 
+//console.log(10n - 4)//error
+console.log(parseInt(10n) - 4) // преобразовать
+console.log(10n - BigInt(4)) // преобразовать
+console.log(5n / 2n) // 2.5 - отсекается дробная = 2 */
