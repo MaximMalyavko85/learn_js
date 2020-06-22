75ce26fbd0fc82d5f07263ee54a4721d26d14105
Maxim Malyavko <maximmalyavko@yandex.ru>
2020-06-22 18:28:25 +0300

    Добавлена функция values для обьектов

--- 06_objects.js
+++ 06_objects.js
@@ -44,6 +44,14 @@ Object.keys(person).forEach((key) => {
   console.log(person[key])
 }) */
 
+const object1 = {
+  a: 'somestring',
+  b: 42,
+  c: false
+};
+
+console.log(Object.values(object1));
+// expected output: Array ["somestring", 42, false]
 
 
 //КОНТЕКСТ
